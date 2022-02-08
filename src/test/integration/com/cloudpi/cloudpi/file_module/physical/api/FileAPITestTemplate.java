cc57b46d55e7a5bc42d1b07e93ce56b604bd8b1b
Kamil Wójcik <wojcikakamil@gmail.com>
2022-02-08 16:35:55 +0100

    added Test for uploadNewFile in FileApi

--- src/test/integration/com/cloudpi/cloudpi/file_module/physical/api/FileAPITestTemplate.java
+++ src/test/integration/com/cloudpi/cloudpi/file_module/physical/api/FileAPITestTemplate.java
@@ -1,14 +1,19 @@
 package com.cloudpi.cloudpi.file_module.physical.api;
 
 import com.cloudpi.cloudpi.file_module.virtual_filesystem.pojo.FileType;
+import com.cloudpi.cloudpi.user.api.requests.PostUserRequest;
 import com.cloudpi.cloudpi.utils.ControllerTest;
+import com.cloudpi.cloudpi.utils.UserAPIUtils;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 
@@ -18,17 +23,40 @@ public class FileAPITestTemplate {
     @Autowired
     protected MockMvc mockMvc;
     @Value("${cloud-pi.storage.mock.save-files-dir}")
-    protected String storageMockPath;
+    private String storageMockPath;
 
     protected final String apiAddress = "/files/";
 
+    protected final ImmutableList<PostUserRequest> userRequestList = ImmutableList.of(
+            new PostUserRequest(
+                    "bob",
+                    "bob",
+                    null,
+                    "P@ssword123"
+            ),
+            new PostUserRequest(
+                    "Alice",
+                    "Alice",
+                    null,
+                    "P@ssword321"
+            )
+    );
+
+    protected void addUsersToDB() throws Exception {
+        var userApiUtils = new UserAPIUtils(mockMvc);
+
+        for (var userRequest : userRequestList) {
+            userApiUtils.addUserToDB(userRequest);
+        }
+    }
+
     protected void clearStorageDirectory() {
-        var path = Paths.get(storageMockPath);
+        var path = getStoragePath();
         var parentDir = path.toFile();
         var filesInDir = parentDir.listFiles();
 
         if (!parentDir.exists() || !parentDir.isDirectory() || filesInDir == null) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("Path: " + path + " does not exist or is not directory");
         }
 
         for (var file : filesInDir) {
@@ -50,8 +78,22 @@ public class FileAPITestTemplate {
     }
 
     protected boolean fileExist(String fileId) {
-        var storagePath = Paths.get(storageMockPath + "/" + fileId);
+        var storagePath = Paths.get(getStoragePath() + "/" + fileId);
         return storagePath.toFile().exists();
+    }
+
+    protected boolean fileExist(UUID fileId) {
+        var storagePath = Paths.get(getStoragePath() + "/" + fileId);
+        return storagePath.toFile().exists();
+    }
+
+    protected Path getStoragePath() {
+        if (storageMockPath.startsWith("~")) {
+            var path = System.getProperty("user.home") + storageMockPath.substring(1);
+            return Paths.get(path);
+        }
+
+        return Paths.get(storageMockPath);
     }
 
 }
---
+++ src/test/integration/com/cloudpi/cloudpi/file_module/physical/api/endpoint_tests/TestUploadNewFile.java
@@ -0,0 +1,60 @@
+package com.cloudpi.cloudpi.file_module.physical.api.endpoint_tests;
+
+import com.cloudpi.cloudpi.config.security.Role;
+import com.cloudpi.cloudpi.file_module.physical.api.FileAPITestTemplate;
+import com.cloudpi.cloudpi.file_module.virtual_filesystem.dto.FileInfoDTO;
+import com.cloudpi.cloudpi.utils.ControllerTest;
+import com.cloudpi.cloudpi.utils.mock_mvc_users.WithUser;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static com.cloudpi.cloudpi.utils.MockMvcUtils.getBody;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ControllerTest
+public class TestUploadNewFile extends FileAPITestTemplate {
+
+    @BeforeEach
+    void setUp() throws Exception {
+        addUsersToDB();
+        clearStorageDirectory();
+    }
+
+    @AfterAll
+    void clearStorage() throws Exception {
+        clearStorageDirectory();
+    }
+
+    @Test
+    @WithUser(username = "bob", authorities = Role.user)
+    void should_save_given_file() throws Exception {
+        //given
+//        var txtFile = getClass().getClassLoader().getResourceAsStream("test_files/text.txt");
+        var file = new MockMultipartFile(
+                "file",
+                "text.txt",
+                "text/plan",
+                "Hello, World!".getBytes());
+
+        //when
+        var response = mockMvc.perform(
+                        multipart(apiAddress + "file")
+                                .file(file)
+                                .param("filepath", "bob/text.txt")
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse();
+
+        var fileInfo = getBody(response, FileInfoDTO.class);
+
+
+        //then
+        assert fileExist(fileInfo.getPubId());
+
+    }
+
+}
