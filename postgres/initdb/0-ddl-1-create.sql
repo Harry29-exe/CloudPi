ada86e85ff8a7d0863cce8d152fc7da7d871a3ca
Kamil Wójcik <wojcikakamil@gmail.com>
2022-02-25 00:09:42 +0100

    fixed ddl

--- postgres/initdb/0-ddl-1-create.sql
+++ postgres/initdb/0-ddl-1-create.sql
@@ -81,23 +81,43 @@ create table users
     username varchar(255) not null,
     primary key (id)
 );
-alter table if exists drive add constraint UK_jxrd4wbx6353wfridjbutg5i5 unique (path);
-alter table if exists file_info add constraint UK_pi4obrmd1wc2h2yyjawwuccuc unique (path);
-alter table if exists file_info add constraint UK_5igikvx316l8nlbtsby5tf41e unique (pub_id);
-alter table if exists filesystems add constraint UK_n70l2fs0utyi933krv5u5g68i unique (user_id);
-alter table if exists users add constraint UK_lfiagvamoj0k9pprynqpmi9i9 unique (pub_id);
-alter table if exists users add constraint UK_r43af9ap4edm43mmtq01oddj6 unique (username);
-alter table if exists file_ancestor add constraint FK7173dv1t7m43v1scn47nbxksx foreign key (ancestor) references file_info;
-alter table if exists file_ancestor add constraint FKegg1kearh0ieqi2fkfpgjsa8n foreign key (file_id) references file_info;
-alter table if exists file_details add constraint FK175bqent5becveiqnop8t1r8v foreign key (file_id) references file_info;
-alter table if exists file_info add constraint FKhiqhfxxph5u62xkqm243wgs4l foreign key (drive_files) references drive;
-alter table if exists file_info add constraint FK4qoiilgpiu5qg5asnqgi84jnx foreign key (parent_id) references file_info;
-alter table if exists file_info add constraint FK35j0h3g2aiqlstxcefqc4jo6b foreign key (root_id) references filesystems;
-alter table if exists file_permission add constraint FKn3kpjngwoce5euneoca0mdrpl foreign key (file_id) references file_info;
-alter table if exists file_permission add constraint FKp43uy7esj9vsuqvotjod8i0ty foreign key (user_id) references users;
-alter table if exists file_permission add constraint FKjpfsll8d9e62tmhyc5jo7yjj5 foreign key (permissions) references file_info;
-alter table if exists filesystems add constraint FKhf1rlrrl3r5qoewirr7hagfb6 foreign key (user_id) references users;
-alter table if exists filesystems add constraint FKgdl0qxqsj86u4ec55pgcg7vl3 foreign key (root_directory) references file_info;
-alter table if exists role_entity add constraint FKih8eyutwr9a19m9q2ihue15r0 foreign key (user_id) references users;
-alter table if exists user_details add constraint FKdic9c3qbc8w2vp8humyr03m1m foreign key (profile_picture_id) references file_info;
-alter table if exists user_details add constraint FKicouhgavvmiiohc28mgk0kuj5 foreign key (user_id) references users;
+alter table if exists drive
+    add constraint UK_jxrd4wbx6353wfridjbutg5i5 unique (path);
+alter table if exists file_info
+    add constraint UK_pi4obrmd1wc2h2yyjawwuccuc unique (path);
+alter table if exists file_info
+    add constraint UK_5igikvx316l8nlbtsby5tf41e unique (pub_id);
+alter table if exists filesystems
+    add constraint UK_n70l2fs0utyi933krv5u5g68i unique (user_id);
+alter table if exists users
+    add constraint UK_lfiagvamoj0k9pprynqpmi9i9 unique (pub_id);
+alter table if exists users
+    add constraint UK_r43af9ap4edm43mmtq01oddj6 unique (username);
+alter table if exists file_ancestor
+    add constraint FK7173dv1t7m43v1scn47nbxksx foreign key (ancestor) references file_info;
+alter table if exists file_ancestor
+    add constraint FKegg1kearh0ieqi2fkfpgjsa8n foreign key (file_id) references file_info;
+alter table if exists file_details
+    add constraint FK175bqent5becveiqnop8t1r8v foreign key (file_id) references file_info;
+alter table if exists file_info
+    add constraint FKhiqhfxxph5u62xkqm243wgs4l foreign key (drive_files) references drive;
+alter table if exists file_info
+    add constraint FK4qoiilgpiu5qg5asnqgi84jnx foreign key (parent_id) references file_info;
+alter table if exists file_info
+    add constraint FK35j0h3g2aiqlstxcefqc4jo6b foreign key (root_id) references filesystems;
+alter table if exists file_permission
+    add constraint FKn3kpjngwoce5euneoca0mdrpl foreign key (file_id) references file_info;
+alter table if exists file_permission
+    add constraint FKp43uy7esj9vsuqvotjod8i0ty foreign key (user_id) references users;
+alter table if exists file_permission
+    add constraint FKjpfsll8d9e62tmhyc5jo7yjj5 foreign key (permissions) references file_info;
+alter table if exists filesystems
+    add constraint FKhf1rlrrl3r5qoewirr7hagfb6 foreign key (user_id) references users;
+alter table if exists filesystems
+    add constraint FKgdl0qxqsj86u4ec55pgcg7vl3 foreign key (root_directory) references file_info;
+alter table if exists role_entity
+    add constraint FKih8eyutwr9a19m9q2ihue15r0 foreign key (user_id) references users;
+alter table if exists user_details
+    add constraint FKdic9c3qbc8w2vp8humyr03m1m foreign key (profile_picture_id) references file_info;
+alter table if exists user_details
+    add constraint FKicouhgavvmiiohc28mgk0kuj5 foreign key (user_id) references users;
--- postgres/initdb/1-dml-0-admin-user.sql
+++ postgres/initdb/1-dml-0-admin-user.sql
@@ -1,9 +1,12 @@
-INSERT INTO users VALUES
+INSERT INTO users
+VALUES
 --P@ssword123
 (1, false, '$2a$10$1H1Ro.4Fu2p3nLcMGXN/ge.PBDOKdzUWZoVpudYNLmp1A6RT3N0m6', gen_random_uuid(), 'admin');
 
-INSERT INTO user_details VALUES
-(1, NULL, 'admin', NULL);
+INSERT INTO user_details
+VALUES (1, NULL, 'admin', NULL);
 
-INSERT INTO user_entity_roles VALUES
-(1, 0), (1, 1), (1, 2);
+INSERT INTO role_entity(id, user_id, role)
+VALUES (1, 1, 0),
+       (2, 1, 1),
+       (3, 1, 2);
